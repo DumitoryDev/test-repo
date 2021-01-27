c31a94ec7ea03a7271af23c221b52ede3d4c77f2
Dumitory <blackmesa2018@gmailcom>
2021-01-27 16:49:23 +0300

    Add factorial func

--- main.cpp
+++ main.cpp
@@ -1,19 +1,32 @@
 #include <iostream>
 #include <exception>
 
+constexpr size_t get_factorial(size_t size)
+{
+
+  size_t res{};
+  for (size_t i = 1; i <= size; i++)
+  {
+    res *= i;
+  }
+
+  return res;
+  
+}
+
 int main(void)
 {
-   try
-   {
-     std::cout << "Hello, world!" << std::endl;       
-   }
-   catch(const std::exception & error)
-   {
-     std::cerr << error.what() << std::endl;
-   }
-   catch(...)
-   {
-   }
+  try
+  {
+    std::cout << "Hello, world!" << std::endl;
+  }
+  catch (const std::exception &error)
+  {
+    std::cerr << error.what() << std::endl;
+  }
+  catch (...)
+  {
+  }
 
-   return 0;
+  return 0;
 }
