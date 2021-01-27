24cc99b3e30cc1227397117cee742d3f2e8f0534
Dumitory <blackmesa2018@gmailcom>
2021-01-27 15:00:43 +0300

    Add try and catch

--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
+#include <exception>
 
 int main(void)
 {
+   try
+   {
+       
+   }
+   catch(std::exception const & error)
+   {
+     std::cerr << error.what() << std::endl;
+   }
+
    return 0;
 }
